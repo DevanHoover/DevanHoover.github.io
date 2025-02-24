38d776020372785feffb68f0756350e0ef826232
DevanHoover <142552755+DevanHoover@users.noreply.github.com>
2025-02-24 13:46:23 -0600

    attempt 3

--- Weather App/app.js
+++ Weather App/app.js
@@ -34,7 +34,7 @@ async function getWeather() {
 
         if(weatherType == 0) {
             document.querySelector('.weatherType').innerHTML = 'Clear Skies';
-            animation.src = 'images/sun.gif';
+            animation.src = './images/sun.gif';
         } else if(weatherType == 1) {
             document.querySelector('.weatherType').innerHTML = 'Mostly Sunny';
             animation.src = 'images/sun.gif';
