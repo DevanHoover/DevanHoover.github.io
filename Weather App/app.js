8d4337df1c97649eacc579cf0b0ecb8b58e4d637
DevanHoover <142552755+DevanHoover@users.noreply.github.com>
2025-02-24 11:09:31 -0600

    Added images and Design as well as functionality

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,4 +1,4 @@
-apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=36.1628&longitude=-85.5016&hourly=temperature_2m,relative_humidity_2m,precipitation,visibility&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto'
+apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=36.1628&longitude=-85.5016&hourly=temperature_2m,relative_humidity_2m,precipitation,weather_code,visibility&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=auto'
 
 async function getWeather() {
     try {
@@ -9,25 +9,112 @@ async function getWeather() {
         const data = await response.json();
         console.log("Weather Stuff:", data);
 
-        const temp = data.hourly.temperature_2m[0];
+        const temp = data.hourly.temperature_2m[0].toFixed(0) + '°';
         const humidity = data.hourly.relative_humidity_2m[0];
 
-        const precipitation = data.hourly.precipitation[0];
+        const precipitation = data.hourly.precipitation[0].toFixed(1);
         if(precipitation == null || precipitation == undefined) {
             precipitation = '0%';
         }
-        const visibility = (data.hourly.visibility[0] * 0.000621371).toFixed(2); // converts meters to miles 
+        const visibility = (data.hourly.visibility[0] * 0.000621371).toFixed(0); // converts meters to miles 
+
+        const weatherType = data.hourly.weather_code[0];
         
 
 
 
-        document.querySelector('.temp').innerHTML = temp + '°F';
+        document.querySelector('.temp').innerHTML = temp;
         document.querySelector('.humidity').innerHTML = humidity + '%';
         document.querySelector('.precipitation').innerHTML = precipitation + ' in';
         document.querySelector('.visibility').innerHTML = visibility + ' mi';
+        document.querySelector('.weatherType').innerHTML = weatherType;
+        
+        animation = document.querySelector('#right-container img');
+
+        if(weatherType == 0) {
+            document.querySelector('.weatherType').innerHTML = 'Clear Skies';
+            animation.src = 'images/sun.gif';
+        } else if(weatherType == 1) {
+            document.querySelector('.weatherType').innerHTML = 'Mostly Sunny';
+            animation.src = 'images/sun.gif';
+        }else if(weatherType == 2) {
+            document.querySelector('.weatherType').innerHTML = 'Partly Cloudy';
+            animation.src = 'images/cloudy.gif';
+        } else if(weatherType == 3) {
+            document.querySelector('.weatherType').innerHTML = 'Overcast';
+            animation.src = 'images/clouds.gif';
+        } else if(weatherType == 48 || 45) {
+            document.querySelector('.weatherType').innerHTML = 'Fog';
+            animation.src = 'images/foggy.gif';
+        } 
+        else if(weatherType == 61 || 51 || 53 || 55) {
+            document.querySelector('.weatherType').innerHTML = 'Light Rain';
+            animation.src = 'images/rain.gif';
+        } else if(weatherType == 63) {
+            document.querySelector('.weatherType').innerHTML = 'Moderate Rain';
+            animation.src = 'images/rain.gif';
+        } else if(weatherType == 65) {
+            document.querySelector('.weatherType').innerHTML = 'Heavy Rain';
+            animation.src = 'images/rain.gif';
+        } else if(weatherType == 66 || 67 || 56 || 57) {
+            document.querySelector('.weatherType').innerHTML = 'Freezing Rain';
+            animation.src = 'images/hail.gif';
+        } else if(weatherType == 71) {
+            document.querySelector('.weatherType').innerHTML = 'Light Snow';
+            animation.src = 'images/snow.gif';
+        } else if(weatherType == 73) {
+            document.querySelector('.weatherType').innerHTML = 'Moderate Snow';
+            animation.src = 'images/snow.gif';
+        } else if(weatherType == 75 || 77) {
+            document.querySelector('.weatherType').innerHTML = 'Heavy Snow';
+            animation.src = 'images/snow.gif';
+        } else if(weatherType == 95) {
+            document.querySelector('.weatherType').innerHTML = 'Thunderstorms';
+            animation.src = 'images/storm.gif';
+        } else if(weatherType == 80) {
+            document.querySelector('.weatherType').innerHTML = 'Light Showers';
+            animation.src = 'images/rain.gif';
+        } else if(weatherType == 81) {
+            document.querySelector('.weatherType').innerHTML = 'Moderate Showers';
+            animation.src = 'images/rain.gif';
+        } else if(weatherType == 82) {
+            document.querySelector('.weatherType').innerHTML = 'Heavy Showers';
+            animation.src = 'images/rain.gif';
+        } else if(weatherType == 85) {
+            document.querySelector('.weatherType').innerHTML = 'Light Snow Showers';
+            animation.src = 'images/snow.gif';
+        } else if(weatherType == 86) {
+            document.querySelector('.weatherType').innerHTML = 'Heavy Snow Showers';
+            animation.src = 'images/snow.gif';
+        } else if(weatherType == 96) {
+            document.querySelector('.weatherType').innerHTML = 'Light Hail Showers';
+            animation.src = 'images/hail.gif';
+        } else if (weatherType == 99) {
+            document.querySelector('.weatherType').innerHTML = 'Heavy Hail Showers';
+            animation.src = 'images/hail.gif';
+        }   
+    
+        
+
+        setBackgroundImg(temp);
 
     } catch (notGood) {
         console.error("Weather data no good:", notGood);
+    }
+}
+
+function setBackgroundImg(temp) {
+
+    document.getElementById('hot').classList.add('d-none');
+    document.getElementById('cold').classList.add('d-none');
+
+    if(temp >= 80) {
+        document.getElementById('hot').style.backgroundImage = "url('images/hot.png')";
+        document.getElementById('hot').classList.remove('d-none');
+    }
+else if(temp <= 40) {
+    document.getElementById('cold').style.backgroundImage = "url('images/temperature.png')";
+    document.getElementById('cold').classList.remove('d-none');
     }
 }
 
